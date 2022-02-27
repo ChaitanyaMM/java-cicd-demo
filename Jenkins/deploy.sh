69dbb4702fb48a1ae02c2622e712f54afe188d7e
chaitanyaMM <krishnachaitu.17@gmail.com>
2022-02-27 17:10:14 +0530

    deloy.sh

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -3,8 +3,8 @@
 
 IMAGE="java-cicd-demo"
 
-ssh -i $aws_kv ubuntu@ec2-3-6-94-212.ap-south-1.compute.amazonaws.com
-mkdir chaitanya && cd
+ssh -i $aws_kv ec2-user@ec2-3-6-94-212.ap-south-1.compute.amazonaws.com
+mkdir chaitanya2 && cd
 docker login -u chaitu465 -p $PASS
 docker pull chaitu465/$IMAGE:$BUILD_TAG
-
+echo "done deploying!."
