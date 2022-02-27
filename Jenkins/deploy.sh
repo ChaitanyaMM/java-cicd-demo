256ac71a48ce9d4c672b913538ff288e62e4d3b4
chaitanyaMM <krishnachaitu.17@gmail.com>
2022-02-27 17:36:25 +0530

    deploy file

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -4,7 +4,12 @@
 IMAGE="java-cicd-demo"
 
 ssh -i $aws_kv ubuntu@ec2-3-6-94-212.ap-south-1.compute.amazonaws.com
-mkdir chaitanya2
+echo 'cloning app ! ...'
+  checkout([
+        $class: 'GitSCM', branches: [[name: '*/main']],
+        userRemoteConfigs: [[url: 'https://github.com/ChaitanyaMM/java-cicd-demo-app.git',credentialsId: '$gitCredentials']]
+          ])
+echo "hello" >> chaitanya/file.txt
 docker login -u chaitu465 -p $PASS
 docker pull chaitu465/$IMAGE:$BUILD_TAG
 echo "done deploying!."
