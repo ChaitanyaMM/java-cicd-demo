08df716ea62070f6de1b590067aae852635fdfbb
chaitanyaMM <krishnachaitu.17@gmail.com>
2022-02-27 11:11:47 +0530

    changin the build

--- Jenkins/build.sh
+++ Jenkins/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Copy the new jar to the build location
-cd /home/chaitanya/.jenkins/workspace/java-cicd-demo/ && mvn clean install
+# go to the workspace and start execting the process
 echo "****************************"
 echo "** Building Maven jar ***"
 echo "****************************"
+
+cd /home/chaitanya/.jenkins/workspace/java-cicd-demo/ && mvn clean install
+
+echo "****************************"
+echo "**Running Maven jar ***"
+echo "****************************"
+cd  target && java -jar java-cicd-demo-0.0.1-SNAPSHOT.jar
