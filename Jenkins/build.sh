b6e280587855330968a1982c82dd0fbb38240995
chaitanyaMM <krishnachaitu.17@gmail.com>
2022-02-27 15:15:11 +0530

    commenting the run

--- Jenkins/build.sh
+++ Jenkins/build.sh
@@ -10,4 +10,4 @@ cd /home/chaitanya/.jenkins/workspace/java-cicd-demo/ && mvn clean install
 echo "****************************"
 echo "**Running Maven jar ***"
 echo "****************************"
-cd  target && java -jar java-cicd-demo-0.0.1-SNAPSHOT.jar
+#cd  target && java -jar java-cicd-demo-0.0.1-SNAPSHOT.jar
