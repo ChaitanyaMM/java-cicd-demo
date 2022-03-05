be9a9b4db00c1f6b19eaacd1f738ccc9c9df2d55
chaitanyaMM <krishnachaitu.17@gmail.com>
2022-03-05 10:47:38 +0530

    adding jfrog credentials

--- Jenkins/jfrog.sh
+++ Jenkins/jfrog.sh
@@ -5,7 +5,9 @@ echo "** pushing docker image to jfrog  Artifactory**********"
 echo "***************************"
 
 
-docker login ${jfrog_server_name}.jfrog.io
+docker login ${jfrog_server_name}.jfrog.io -u $JFROG_USERNAME -p $JFROG_PASS
 
-docker tag d70ea63efeee ${jfrog_server_name}/default-docker-virtual/chaitu465/java-cicd-demo:jenkins-java-cicd-demo-62
-docker push ${jfrog_server_name}/default-docker-virtual/chaitu465/java-cicd-demo:jenkins-java-cicd-demo-62
+docker tag 7f80a17b7388 ${jfrog_server_name}/default-docker-virtual/chaitu465/java-cicd-demo:jenkins-java-cicd-demo-57
+docker push ${jfrog_server_name}/default-docker-virtual/chaitu465/java-cicd-demo:jenkins-java-cicd-demo-57
+
+echo "pushed image to artifactory ${jfrog_server_name}."
